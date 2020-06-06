3fba31259871e51578b6c74d76e52440e87ba5d6
nirojuderavi <nirojuderavi@outlook.com>
2020-06-06 09:53:23 +0100

    sane as before

--- MicroRabbitTakeaway/MicroRabbitTakeaway.Infra.Bus/RabbitMQBus.cs
+++ MicroRabbitTakeaway/MicroRabbitTakeaway.Infra.Bus/RabbitMQBus.cs
@@ -1,11 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using MediatR;
 using MicroRabbitTakeaway.Domain.Core.Bus;
+using MicroRabbitTakeaway.Domain.Core.Commands;
+using MicroRabbitTakeaway.Domain.Core.Events;
 
 namespace MicroRabbitTakeaway.Infra.Bus
 {
     public sealed class RabbitMQBus : IEventBus
     {
+        private readonly IMediator _mediator;
+
+        public Task SendCommand<T>(T command) where T : Command
+        {
+            return _mediator.Send(command);
+        }
+
+        public void Publish<T>(T @event) where T : Event
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>
+        {
+            throw new NotImplementedException();
+        }
     }
 }
