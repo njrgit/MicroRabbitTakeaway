1c4dc467d633a1bf5acd75f5dac9f94ee8946f05
nirojuderavi <nirojuderavi@outlook.com>
2020-06-06 09:52:10 +0100

    Send Command using MediatR

---
+++ MicroRabbitTakeaway/MicroRabbitTakeaway.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabbitTakeaway.Domain.Core.Bus;
+
+namespace MicroRabbitTakeaway.Infra.Bus
+{
+    public sealed class RabbitMQBus : IEventBus
+    {
+    }
+}
